915611ffea04fe4108779b841a01e5652996bc00
Ahmad Abdullah <ahmadabdullah.formal@gmail.com>
2026-09-20 15:37:51 +0300

    feat(API:Infrastructure): Add PagedList and PaginationParams

---
+++ Infrastructure/Common/PagedList.cs
@@ -0,0 +1,33 @@
+﻿namespace Infrastructure.Common;
+
+using Microsoft.EntityFrameworkCore;
+public class PagedList<T>
+{
+    public List<T> Items { get; set; } = []; 
+    public int CurrentPage { get; set; } 
+    public int TotalPages { get; set; } 
+    public int TotalCount { get; set; } 
+    public bool HasNextPage => CurrentPage < TotalPages; 
+    public bool HasPreviousPage => CurrentPage > 1;
+
+    public static async Task<PagedList<T>> CreateAsync(
+        IQueryable<T> query,
+        int page,
+        int pageSize,
+        CancellationToken ct = default
+        )
+    {
+        var totalCount = await query.CountAsync(ct);
+        var items =
+            await query.Skip((page - 1) * pageSize) 
+            .Take(pageSize)
+            .ToListAsync(ct);
+        return new PagedList<T>
+        {
+            Items = items,
+            CurrentPage = page,
+            TotalPages = (int)Math.Ceiling(totalCount / (double)pageSize), 
+            TotalCount = totalCount
+        };
+    }
+}
---
+++ Infrastructure/Common/PaginationParams.cs
@@ -0,0 +1,14 @@
+﻿namespace Infrastructure.Common;
+
+public class PaginationParams
+{
+    private const int MaxPageSize = 50; 
+    private int _pageSize = 10; 
+    public int Page { get; set; } = 1;
+
+    public int PageSize
+    {
+        get => _pageSize;
+        set => _pageSize = (value > MaxPageSize) ? MaxPageSize : value; // Enforce max limit
+    }
+}
