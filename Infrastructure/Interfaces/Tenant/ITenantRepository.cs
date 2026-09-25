b57ffe824690922030f947a40cc1e16c9d357221
Ahmad Abdullah <ahmadabdullah.formal@gmail.com>
2026-09-25 10:51:38 +0300

    feat(API): Add IsTenantExistsById on Repository

--- Infrastructure/Interfaces/Tenant/ITenantRepository.cs
+++ Infrastructure/Interfaces/Tenant/ITenantRepository.cs
@@ -1,5 +1,5 @@
 ﻿namespace Infrastructure.Interfaces.Tenant;
 public interface ITenantRepository : IRepository<TenantEntity>
 {
-     Task<bool> IsExistsByIdAsync(string id, CancellationToken ct);
+     Task<bool> IsTenantExistsByIdAsync(string id, CancellationToken ct);
 }
--- Infrastructure/Services/Tenant/TenantRepository.cs
+++ Infrastructure/Services/Tenant/TenantRepository.cs
@@ -5,7 +5,7 @@ namespace Infrastructure.Services.Tenant;
 public class TenantRepository : Repository<TenantEntity>, ITenantRepository
 {
     public TenantRepository(AppDbContext dbContext) : base(dbContext) { }
-    public async Task<bool> IsExistsByIdAsync(string id, CancellationToken ct)
+    public async Task<bool> IsTenantExistsByIdAsync(string id, CancellationToken ct)
         => await DbSet.AnyAsync(e => e.Id == id, ct);
 
 
